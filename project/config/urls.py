a460fec514cc7f06e13340b1462dd88678c91688
BetterCallAndrulek <ddemon245@gmail.com>
2024-05-04 20:19:24 +0700

    hw20.1

--- project/config/urls.py
+++ project/config/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
-
-app_name = CatalogConfig.name
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
-]
+    path('admin/', admin.site.urls),
+    path('', include('catalog.urls', namespace='catalog'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
