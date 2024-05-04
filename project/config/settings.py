5913dc41f80a6f4800f4a3e696778844409b3e48
BetterCallAndrulek <ddemon245@gmail.com>
2024-05-05 01:02:21 +0700

    hw20.1

--- project/config/settings.py
+++ project/config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': admin,
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': 5432,
     }
--- project/config/urls.py
+++ project/config/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from catalog.apps import CatalogConfig
+from catalog.views import home, contacts
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('catalog.urls', namespace='catalog'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts')
+]
